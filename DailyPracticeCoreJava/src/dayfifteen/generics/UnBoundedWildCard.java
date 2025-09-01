531f514515ffaaf2450f406945a2dd576861a2b5
dineshmurugan-26 <dineshmurugan2612@gmail.com>
2025-09-01 11:33:56 +0530

    Day-15:Generics

---
+++ DailyPracticeCoreJava/src/dayfifteen/generics/UnBoundedWildCard.java
@@ -0,0 +1,12 @@
+package dayfifteen.generics;
+
+import java.util.*;
+
+public class UnBoundedWildCard {
+	public static void display(List<?> ll) {
+		for (Object o : ll) {
+			System.out.println(o);
+		}
+	}
+
+}
